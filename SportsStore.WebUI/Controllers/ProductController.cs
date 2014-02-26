72ec67666370e96fef6d1fb303ad27a10bb8803b
dotslashbin <joshuarpf@gmail.com>
2014-02-26 21:15:40 +0800

    first commit

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int pageSize = 4; 
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository; 
+        }
+
+        public ViewResult List(int page = 1)
+        {
+
+            return View(repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * pageSize).Take(pageSize)); 
+        }
+	}
+}
---
+++ SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,34 @@
+﻿using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pageInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int iterator = 1; iterator <= pageInfo.TotalPages; iterator++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(iterator));
+                tag.InnerHtml = iterator.ToString(); 
+                if(iterator == pageInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary"); 
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
