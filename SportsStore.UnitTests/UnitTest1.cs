acab41371cb42221b139d6a7adcd2d018a9cc414
dotslashbin <joshuarpf@gmail.com>
2014-02-27 21:29:03 +0800

    Added Test Can_Create_Categories. Displayed Categories Menu on main page. Added Model wrapper for categories. Added views for categories

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -118,5 +118,32 @@ namespace SportsStore.UnitTests
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2"); 
         }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange - create mock respository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>(); 
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" },
+            }); 
+
+            // Arrange - Create the controller
+            NavController target = new NavController(mock.Object); 
+
+            // Act - get the result of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray(); 
+
+            // Assert
+            Assert.AreEqual(results.Length, 3); 
+            Assert.AreEqual(results[0], "Cat1"); 
+            Assert.AreEqual(results[1], "Cat2"); 
+            Assert.AreEqual(results[2], "Cat3"); 
+        
+        }
     }
 }
