2150d9a7b294f27d894ad1fecef3faf4d8b6a257
dotslashbin <joshuarpf@gmail.com>
2014-02-27 10:43:01 +0800

    Crated Unit test for pagination. Problem found: Where is the page 2 coming from

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers; 
 
 namespace SportsStore.UnitTests
 {
@@ -7,8 +13,31 @@ namespace SportsStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "p1"}, 
+                new Product { ProductID = 2, Name = "p2"}, 
+                new Product { ProductID = 3, Name = "p3"}, 
+                new Product { ProductID = 4, Name = "p4"}, 
+                new Product { ProductID = 5, Name = "p5"}
+                //new Product { ProductID = 6, Name = "p6"}, 
+                //new Product { ProductID = 7, Name = "p7"}, 
+                //new Product { ProductID = 8, Name = "p8"}, 
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.pageSize = 3;
+
+            // Act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model; 
+
+            // Assert
+            Product[] productArray = result.ToArray();
+            Assert.IsTrue(productArray.Length == 2);
+            Assert.AreEqual(productArray[0].Name, "p4");
+            Assert.AreEqual(productArray[1].Name, "p5"); 
         }
     }
 }
