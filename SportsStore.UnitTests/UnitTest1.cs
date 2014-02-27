b732a1d4d4969257257175fb8ffa94fa5360fdce
dotslashbin <joshuarpf@gmail.com>
2014-02-27 10:53:19 +0800

    Added  unit test Can_Generate_Page_Links

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Web.Mvc; 
 using System.Collections.Generic;
 using System.Linq; 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
-using SportsStore.WebUI.Controllers; 
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers; 
 
 namespace SportsStore.UnitTests
 {
@@ -38,6 +41,26 @@ namespace SportsStore.UnitTests
             Assert.IsTrue(productArray.Length == 2);
             Assert.AreEqual(productArray[0].Name, "p4");
             Assert.AreEqual(productArray[1].Name, "p5"); 
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            HtmlHelper htmlHelper = null;
+
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            }; 
+
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            MvcHtmlString result = htmlHelper.PageLinks(pagingInfo, pageUrlDelegate); 
+
+            // Assert 
+            //Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1<a/>" + @"<a class=""btn btn-default"" hnref=""Page2"">2</a>" + @"<a class=""btn btn-default"" hnref=""Page3"">3</a>", result.ToString()); 
         }
     }
 }
